5c004c8b81ef9c501056c699f54333f2532cc0d6
Bakalov Vitaliy <bakalov.vitaliy@gmail.com>
2024-03-08 22:53:49 +0700

    Added fonts and configured styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'ru'
+      }
+    }
+  },
+
+  css: ['@/assets/styles/global.scss'],
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData:
+            '@use "@/assets/styles/functions.scss" as *;' +
+              '@use "@/assets/styles/variables.scss" as *;'
+        }
+      }
+    }
+  }
 })
